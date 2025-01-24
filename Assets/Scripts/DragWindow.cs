6b4ed05d4d4633b4686cd2befd3f357d904f5f49
Malan-Tai <malantai99@gmail.com>
2025-01-25 00:00:46 +0100

    empty script

---
+++ Assets/Scripts/DragWindow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour, IDragHandler
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+}
