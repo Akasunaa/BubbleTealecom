ad99dd00b6c885051ea459e0818b271a00c60963
PtiBouchon <bouchon.sappiness682@passinbox.com>
2025-01-26 02:40:59 +0100

    update too much things

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -7,5 +8,33 @@ using UnityEngine.UI;
 
 public class Ingredient : MonoBehaviour
 {
-    public IngredientState ingredient_state;
+    public IngredientState ingredientState;
+
+    public void Transform(Transformation t, List<Ingredient> otherIngredients = null)
+    {
+        IngredientState newIngredientState;
+        if (otherIngredients != null)
+        {
+            newIngredientState = ingredientState.Transform(t, otherIngredients.Select(i => i.ingredientState).ToList());
+        }
+        else
+        {
+            newIngredientState = ingredientState.Transform(t);
+        }
+
+        if (newIngredientState == null)
+        {
+            Debug.LogError("NO TRANSFORMATION");
+        }
+        else
+        {
+            UpdateValue(newIngredientState);
+        }
+    }
+
+    private void UpdateValue(IngredientState ingredientState)
+    {
+        this.ingredientState = ingredientState;
+        GetComponent<Image>().sprite = ingredientState.sprite;
+    }
 }
--- Assets/Scripts/Machines/SlicerController.cs
+++ Assets/Scripts/Machines/SlicerController.cs
@@ -1,4 +1,9 @@
+using System;
+using UI;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Machines
 {
@@ -7,13 +12,43 @@ namespace Machines
     /// </summary>
     public class SlicerController : BaseMachine
     {
+        [SerializeField] private EmptyableItemSlot _mainItemSlot;
         //TODO : Slider must come backto zero when not pointed by player
-        //Good fucking luck
+        
+        private void Awake()
+        {
+            Assert.IsNotNull(_mainItemSlot);
+        }
+
 
         public override void MachineExecuteButtonCalled()
         {
+            var item = _mainItemSlot.GetItem();
+            if (!item)
+            {
+                return;
+            }
+            var ingredient = item.GetComponent<Ingredient>();
+            if (!ingredient)
+            {
+                Debug.LogError("BIZARRE INGREDIENT (probably the glass)");
+                return;
+            }
 
+            ingredient.Transform(Transformation.Cut);
+            SoundManager.PlaySound(SoundManager.Sound.Slicer);
         }
 
+        public void OnSliderValueChanged(Scrollbar scrollbar)
+        {
+            if (scrollbar.value > 0.0)
+            {
+                if (scrollbar.value > 0.9)
+                {
+                    MachineExecuteButtonCalled();
+                }
+                scrollbar.value = 0.0f;
+            }
+        }
     }
 }
